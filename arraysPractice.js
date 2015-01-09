9c6cece084ac4b5930b1f6841309c4ebdb8fb2e2
Barbara Liau <bj.liau@gmail.com>
2015-01-09 15:19:09 -0700

    Completed divider function

--- arraysPractice.js
+++ arraysPractice.js
@@ -79,8 +79,18 @@ var odds = []
 //Write a function called divider that is given three arguments, nums, evens, and odds.
 //Have divider return an Array with the first item in the array being the evens array 
 //(all the even values from nums) and the second item in the Array being the odds array(all the odd values from nums).
-
-
+var divider = function(nums, evens, odds) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+      evens.push(nums[i]);
+    } else{
+      odds.push(nums[i]);
+    }
+  }
+  nums = [];
+  nums.push(evens, odds);
+  return nums;
+};
 
   //Code Here
 
